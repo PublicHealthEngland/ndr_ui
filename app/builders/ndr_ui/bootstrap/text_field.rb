5c3526bd66f64a8eb39223b313f734851715c994
Maximilian Wasylow <bamwasylow@gmail.com>
2017-03-01 15:37:01 +0000

    Hound-cli compliance

--- app/builders/ndr_ui/bootstrap/text_field.rb
+++ app/builders/ndr_ui/bootstrap/text_field.rb
@@ -12,7 +12,7 @@ module NdrUi
         input_group_opts = options.delete('input_group')
         input_group_class = input_group_opts.delete(:class) unless input_group_opts.nil?
         input_group_opts = (input_group_opts.nil? ? {} : input_group_opts).merge(
-          class: => 'input-group' + (input_group_class.nil? ? '' : ' ' + input_group_class))
+          class: 'input-group' + (input_group_class.nil? ? '' : ' ' + input_group_class))
 
         if prepend.blank? && append.blank?
           super
@@ -20,13 +20,15 @@ module NdrUi
           div_content  = ''.html_safe
 
           unless prepend.blank?
-            div_content << @template.content_tag(:span, prepend, :class => 'input-group-addon').html_safe
+            div_content << @template.content_tag(:span, prepend, 
+              class: 'input-group-addon').html_safe
           end
 
           div_content << text_field_without_inline_errors(method, options)
 
           unless append.blank?
-            div_content << @template.content_tag(:span, append, :class => 'input-group-addon').html_safe
+            div_content << @template.content_tag(:span, append, 
+              class: 'input-group-addon').html_safe
           end
 
           @template.content_tag(:div, div_content, input_group_opts)
