758007da7b705e6516596b952138995f54c7ef9c
timgentry <tpgentry@me.com>
2018-12-17 18:58:41 +0000

    # boyscouting

--- app/builders/ndr_ui/bootstrap/readonly.rb
+++ app/builders/ndr_ui/bootstrap/readonly.rb
@@ -27,38 +27,38 @@ module NdrUi
 
       def self.included(base)
         # These have different signatures, or aren't affected by `readonly`:
-        not_affected = [:label, :fields_for]
-        needs_custom = [:radio_button, :file_field, :hidden_field] +
+        not_affected = %i[label fields_for]
+        needs_custom = %i[radio_button file_field hidden_field] +
                        base.field_helpers_from_form_options_helper
 
         (base.field_helpers - needs_custom - not_affected).each do |selector|
-          class_eval <<-END, __FILE__, __LINE__ + 1
+          class_eval <<-EVAL, __FILE__, __LINE__ + 1
             def #{selector}(method, options = {}, *rest)
               return super unless readonly?
               StaticValue.new(object_name, method, @template, objectify_options(options)).render
             end
-          END
+          EVAL
         end
 
-        [:select, :time_zone_select].each do |selector|
-          class_eval <<-END, __FILE__, __LINE__ + 1
+        %i[select time_zone_select].each do |selector|
+          class_eval <<-EVAL, __FILE__, __LINE__ + 1
             def #{selector}(method, _something, options = {}, *rest)
               return super unless readonly?
               StaticValue.new(object_name, method, @template, objectify_options(options)).render
             end
-          END
+          EVAL
         end
 
-        [:collection_select, :collection_check_boxes, :collection_radio_buttons].each do |selector|
-          class_eval <<-END, __FILE__, __LINE__ + 1
+        %i[collection_select collection_check_boxes collection_radio_buttons].each do |selector|
+          class_eval <<-EVAL, __FILE__, __LINE__ + 1
             def #{selector}(method, collection, value_method, text_method, options = {}, *rest)
               return super unless readonly?
               StaticValue.new(object_name, method, @template, objectify_options(options)).render
             end
-          END
+          EVAL
         end
 
-        class_eval <<-END, __FILE__, __LINE__ + 1
+        class_eval <<-EVAL, __FILE__, __LINE__ + 1
           # grouped_collection_select takes many other arguments
           def grouped_collection_select(method, collection, group_method, group_label_method, option_key_method, option_value_method, options = {}, html_options = {})
             return super unless readonly?
@@ -82,15 +82,15 @@ module NdrUi
           def hidden_field(*)
             super unless readonly?
           end
-        END
+        EVAL
 
-        class_eval <<-END, __FILE__, __LINE__ + 1
+        class_eval <<-EVAL, __FILE__, __LINE__ + 1
           # Allow fields_for to inherit `readonly`:
           def fields_for(record_name, record_object = nil, fields_options = {}, &block)
             fields_options[:readonly] ||= readonly
             super
           end
-        END
+        EVAL
       end
 
       attr_accessor :readonly
